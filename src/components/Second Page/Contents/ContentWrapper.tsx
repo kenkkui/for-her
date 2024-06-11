b726242d46eac7fd9abc9eceab07f5928d458130
kenkkui <thifirstime@gmail.com>
2024-06-11 17:10:52 +0800

    done styling content, 1-2

--- src/components/Second Page/Contents/ContentWrapper.tsx
+++ src/components/Second Page/Contents/ContentWrapper.tsx
@@ -1,21 +1,22 @@
 import { useRef, useEffect } from "react";
 import AudioPlayer from "../AudioPlayer";
 import ContentStructure from "./ContentStructure";
+import BdayLetter from "../BdayLetter";
 
 interface ContentWrapperProps {
   setError: React.Dispatch<React.SetStateAction<string[]>>;
 }
 
 export default function ContentWrapper({ setError }: ContentWrapperProps) {
-  const pathRef = useRef<SVGPathElement>(null);
+  const lineOneRef = useRef<SVGPathElement>(null);
 
   useEffect(() => {
-    if (pathRef.current) {
-      const length = pathRef.current.getTotalLength();
+    if (lineOneRef.current) {
+      const length = lineOneRef.current.getTotalLength();
 
-      pathRef.current.style.setProperty("--path-length", `${length}`);
-      pathRef.current.style.strokeDasharray = `${length} ${length}`;
-      pathRef.current.style.strokeDashoffset = `${length}`;
+      lineOneRef.current.style.setProperty("--path-length", `${length}`);
+      lineOneRef.current.style.strokeDasharray = `${length} ${length}`;
+      lineOneRef.current.style.strokeDashoffset = `${length}`;
     }
   }, []);
 
@@ -52,10 +53,13 @@ export default function ContentWrapper({ setError }: ContentWrapperProps) {
         }
         outerMost={
           <div className="content">
-            <h4>
-              HI KARA
-              <div id="exclamation"></div>
-              <p>
+            <div className="title-container">
+              <h4>
+                HI KARA
+                <div id="exclamation"></div>
+              </h4>
+
+              <p className="subtitle">
                 Since you’re obviously reading this, this is just a short web i
                 made for&nbsp;
                 <span>
@@ -69,7 +73,7 @@ export default function ContentWrapper({ setError }: ContentWrapperProps) {
                       xmlns="http://www.w3.org/2000/svg"
                     >
                       <path
-                        ref={pathRef}
+                        ref={lineOneRef}
                         d="M44.4397 2.04875C36.2598 2.04875 24.9527 0.252429 17.7292 4.80052C16.0407 5.86364 13.021 9.45733 16.7753 9.82708C24.7395 10.6114 33.3027 10.2766 41.2843 9.86377C43.9882 9.72391 51.0384 9.21676 49.026 7.40552C48.1373 6.60575 44.4679 6.37636 44.1462 6.34151C37.067 5.5746 29.9044 5.23242 22.7925 4.98397C16.1791 4.75293 -3.65325 4.78648 2.94306 5.31418C6.17268 5.57255 9.50153 5.35087 12.7393 5.35087"
                         stroke="#D6FFF8"
                         stroke-width="3"
@@ -79,7 +83,7 @@ export default function ContentWrapper({ setError }: ContentWrapperProps) {
                   </div>
                 </span>
               </p>
-            </h4>
+            </div>
 
             <p className="text">
               I coded and designed everything BASTA I DID EVERYTHING , so please
@@ -136,16 +140,20 @@ export default function ContentWrapper({ setError }: ContentWrapperProps) {
         id="content-2"
         outer={
           <div className="content">
-            <h6>SO, first of all</h6>
+            <h6>
+              <div id="number-one">
+                <div className="number"></div>
+                <div className="circle"></div>
+              </div>
+              SO, first of all
+              <div id="arrow-one"></div>
+            </h6>
 
-            <div id="bday-letter">
-              <h5>
-                Happy Birthday&nbsp;<span>Kara</span>
-              </h5>
-            </div>
+            <BdayLetter />
           </div>
         }
       />
+      <ContentStructure id="content-3" />
     </>
   );
 }
