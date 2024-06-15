345bc0603fbe0c05f99ceebe129642e4096aacbb
kenkkui <thifirstime@gmail.com>
2024-06-15 11:30:59 +0800

    fix code from position sticky issue

--- src/components/Second-Page/ContentsWrapper.tsx
+++ src/components/Second-Page/ContentsWrapper.tsx
@@ -1,9 +1,3 @@
-import { useRef, useEffect } from "react";
-import ContentStructure from "./ContentSructure";
-import BdayLetter from "./BdayLetter";
-
-import ArrowThree from "../Svg-components/ArrowThree";
-import TextColumns from "./TextColumns";
 import ContentStructure1 from "./Contents/ContentStructure1";
 import ContentStructure2 from "./Contents/ContentStructure2";
 import ContentStructure3 from "./Contents/ContentStructure3";
@@ -15,30 +9,9 @@ interface ContentWrapperProps {
 }
 
 export default function ContentWrapper({ setError }: ContentWrapperProps) {
-  const contentsRef = useRef<HTMLElement>(null);
-
-  useEffect(() => {
-    const contentElem = contentsRef.current as HTMLElement;
-    if (contentElem) {
-      contentElem.addEventListener("scroll", handleScroll);
-      return () => {
-        contentElem.removeEventListener("scroll", handleScroll);
-      };
-    }
-  }, []);
-
-  function handleScroll() {
-    const element = contentsRef.current as HTMLElement;
-    const { scrollTop, scrollHeight, clientHeight } = element;
-    // Remove decimal for scrolltop, and minus one px for scroll height for some reason, i dont undertsand but if it work -- dont touch it
-    if (Math.trunc(scrollTop) + clientHeight === scrollHeight - 1) {
-      console.log("sa bottom na ako");
-    }
-  }
-
   return (
     <>
-      <section className="contents-container" ref={contentsRef}>
+      <section className="contents-container">
         <ContentStructure1 setError={setError} />
         <ContentStructure2 />
         <ContentStructure3 />
