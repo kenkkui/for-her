632253f6df4454c1b90701ac2c66baf549dc3e89
kenkkui <thifirstime@gmail.com>
2024-06-18 23:25:58 +0800

    finishing up

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import FirstPage from "./components/First-Page/FirstPage";
 import SecondPage from "./components/Second-Page/SecondPage";
-import MouseMsg from "./components/MouseMsg";
 
 export default function App() {
   return (
